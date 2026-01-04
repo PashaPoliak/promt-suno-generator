0be60d99a608dfd5d397c18dc279833257188bbc
Pavlo Poliak <pavlo_poliak@epam.com>
2026-01-04 02:20:42 +0200

    Add test cases

--- app/tests/test_get_all_playlists.py
+++ app/tests/test_get_all_playlists.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from services.api import fetch_profile_from_suno
-from config.session import get_db
+from config.session import get_db_sqlite
 from models.entities import Base
 
 def test_get_all_playlists():
